c282cbe68f545b6780778c01a6d47c2e706ed097
Jonas Keturka <jonas.keturka@raisin.com>
2024-12-16 23:26:23 +0200

    Sovling part 2 of day 15

--- cmd/day15/day15.go
+++ cmd/day15/day15.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
+
+type box struct {
+	x1, x2, y int
+}
 
 func process(input []string) int {
 	var data [][]rune
@@ -30,7 +35,46 @@ func process(input []string) int {
 	return calcScore(data)
 }
 
-func move(data [][]rune, direction rune) {
+func processWide(input []string) int {
+	var data [][]rune
+	space := 0
+	for i := 0; input[i] != ""; i++ {
+		var row []rune
+		for _, c := range input[i] {
+			if c == '#' {
+				row = append(row, '#')
+				row = append(row, '#')
+			} else if c == '@' {
+				row = append(row, '@')
+				row = append(row, '.')
+			} else if c == '.' {
+				row = append(row, '.')
+				row = append(row, '.')
+			} else if c == 'O' {
+				row = append(row, '[')
+				row = append(row, ']')
+			}
+
+		}
+		data = append(data, row)
+		space = i + 1
+	}
+
+	var directions []rune
+
+	for i := space + 1; i < len(input); i++ {
+		for _, c := range input[i] {
+			directions = append(directions, c)
+		}
+	}
+
+	for _, d := range directions {
+		moveWide(data, d)
+	}
+	return calcScore(data)
+}
+
+func moveWide(data [][]rune, direction rune) {
 	// print(data)
 	// fmt.Println("Moving", string(direction))
 	robotX, robotY := findRobot(data)
@@ -46,17 +90,79 @@ func move(data [][]rune, direction rune) {
 		}
 		push(path)
 	} else if direction == '^' {
-		for i := robotY; data[i][robotX] != '#'; i-- {
-			path = append(path, &data[i][robotX])
+		boxes := make(map[box]bool)
+		if canPushBoxesUp(data, robotX, robotY-1, boxes) {
+			var boxesSlice []box
+			for key := range boxes {
+				boxesSlice = append(boxesSlice, key)
+			}
+			pushBoxesUp(data, boxesSlice, robotX, robotY)
 		}
-		push(path)
 	} else if direction == 'v' {
-		for i := robotY; data[i][robotX] != '#'; i++ {
-			path = append(path, &data[i][robotX])
+		boxes := make(map[box]bool)
+		if canPushBoxesDown(data, robotX, robotY+1, boxes) {
+			var boxesSlice []box
+			for key := range boxes {
+				boxesSlice = append(boxesSlice, key)
+			}
+			pushBoxesDown(data, boxesSlice, robotX, robotY)
 		}
-		push(path)
 	}
 
+}
+
+func canPushBoxesUp(data [][]rune, x int, y int, boxes map[box]bool) bool {
+	if data[y][x] == '[' {
+		boxes[box{x, x + 1, y}] = true
+		return canPushBoxesUp(data, x, y+1, boxes) && canPushBoxesUp(data, x+1, y+1, boxes)
+	} else if data[y][x] == ']' {
+		boxes[box{x - 1, x, y, y}] = true
+		return canPushBoxesUp(data, x, y+1, boxes) && canPushBoxesUp(data, x-1, y+1, boxes)
+	} else if data[y][x] == '#' {
+		return false
+	}
+	return true
+}
+
+func canPushBoxesDown(data [][]rune, x int, y int, boxes map[box]bool) bool {
+	if data[y][x] == '[' {
+		boxes[box{x, x + 1, y}] = true
+		return canPushBoxesDown(data, x, y-1, boxes) && canPushBoxesDown(data, x+1, y-1, boxes)
+	} else if data[y][x] == ']' {
+		boxes[box{x - 1, x, y}] = true
+		return canPushBoxesDown(data, x, y-1, boxes) && canPushBoxesDown(data, x-1, y-1, boxes)
+	} else if data[y][x] == '#' {
+		return false
+	}
+	return true
+}
+
+func pushBoxesUp(data [][]rune, boxes []box, x int, y int) {
+	sort.Slice(boxes, func(i int, j int) bool {
+		return boxes[i].y < boxes[j].y
+	})
+	for _, b := range boxes {
+		data[b.y-1][b.x1] = '['
+		data[b.y-1][b.x2] = ']'
+		data[b.y][b.x2] = '.'
+		data[b.y][b.x1] = '.'
+	}
+	data[y-1][x] = '@'
+	data[y][x] = '.'
+}
+
+func pushBoxesDown(data [][]rune, boxes []box, x int, y int) {
+	sort.Slice(boxes, func(i int, j int) bool {
+		return boxes[i].y > boxes[j].y
+	})
+	for _, b := range boxes {
+		data[b.y+1][b.x1] = '['
+		data[b.y+1][b.x2] = ']'
+		data[b.y][b.x2] = '.'
+		data[b.y][b.x1] = '.'
+	}
+	data[y+1][x] = '@'
+	data[y][x] = '.'
 }
 
 func findRobot(data [][]rune) (int, int) {
